c17dee856506fbbc21fd55fec4778d710643627c
Matt Jordan <mattistrees@gmail.com>
2020-09-13 17:37:22 -0400

    Make code a little more secure by obfuscating the Wallet address and the EtherScan key

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const fetch = require('node-fetch');
 
 const reporterBot = new Discord.Client();
 
+// APIKEY
+const EtherScanKey = '';
+// Wallet Address
+const DAOWallet = '';
+
 const prefix = '!';
 
 reporterBot.once('ready', () => {
@@ -39,29 +44,29 @@ reporterBot.on('message', async message =>{
     } else if (command === 'wallet'){
 
         try{
-            const ETH = await fetch('https://api.etherscan.io/api?module=account&action=balance&address=0xce5dcffb2aa50deeb229881a294f0b79d41baaeb&tag=latest&apikey=PSHAVQ37TYFF7R369R4X5YJ49Q1WCISQ4Q').then(response => response.json());
+            const ETH = await fetch('https://api.etherscan.io/api?module=account&action=balance&address=' + DAOWallet + '&tag=latest&apikey=' + EtherScanKey).then(response => response.json());
             // contract address for USDC 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48
-            const USDC = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48&address=0xce5dcffb2aa50deeb229881a294f0b79d41baaeb&tag=latest&apikey=PSHAVQ37TYFF7R369R4X5YJ49Q1WCISQ4Q').then(response => response.json());
+            const USDC = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48&address=' + DAOWallet + '&tag=latest&apikey=' + EtherScanKey).then(response => response.json());
             // contract address for MKR 0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2
-            const MKR = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2&address=0xce5dcffb2aa50deeb229881a294f0b79d41baaeb&tag=latest&apikey=PSHAVQ37TYFF7R369R4X5YJ49Q1WCISQ4Q').then(response => response.json());
+            const MKR = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2&address=' + DAOWallet + '&tag=latest&apikey=' + EtherScanKey).then(response => response.json());
             // contract address for BAL 0xba100000625a3754423978a60c9317c58a424e3d
-            const BAL = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xba100000625a3754423978a60c9317c58a424e3d&address=0xce5dcffb2aa50deeb229881a294f0b79d41baaeb&tag=latest&apikey=PSHAVQ37TYFF7R369R4X5YJ49Q1WCISQ4Q').then(response => response.json());
+            const BAL = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xba100000625a3754423978a60c9317c58a424e3d&address=' + DAOWallet + '&tag=latest&apikey=' + EtherScanKey).then(response => response.json());
             // contract address for OMG 0xd26114cd6EE289AccF82350c8d8487fedB8A0C07
-            const OMG = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xd26114cd6EE289AccF82350c8d8487fedB8A0C07&address=0xce5dcffb2aa50deeb229881a294f0b79d41baaeb&tag=latest&apikey=PSHAVQ37TYFF7R369R4X5YJ49Q1WCISQ4Q').then(response => response.json());
+            const OMG = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xd26114cd6EE289AccF82350c8d8487fedB8A0C07&address=' + DAOWallet + '&tag=latest&apikey=' + EtherScanKey).then(response => response.json());
             // contract address for REN 0x408e41876cccdc0f92210600ef50372656052a38
-            const REN = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x408e41876cccdc0f92210600ef50372656052a38&address=0xce5dcffb2aa50deeb229881a294f0b79d41baaeb&tag=latest&apikey=PSHAVQ37TYFF7R369R4X5YJ49Q1WCISQ4Q').then(response => response.json());
+            const REN = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x408e41876cccdc0f92210600ef50372656052a38&address=' + DAOWallet + '&tag=latest&apikey=' + EtherScanKey).then(response => response.json());
             // contract address for ZRX 0xe41d2489571d322189246dafa5ebde1f4699f498
-            const ZRX = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xe41d2489571d322189246dafa5ebde1f4699f498&address=0xce5dcffb2aa50deeb229881a294f0b79d41baaeb&tag=latest&apikey=PSHAVQ37TYFF7R369R4X5YJ49Q1WCISQ4Q').then(response => response.json());
+            const ZRX = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xe41d2489571d322189246dafa5ebde1f4699f498&address=' + DAOWallet + '&tag=latest&apikey=' + EtherScanKey).then(response => response.json());
             // contract address for BAT 0x0d8775f648430679a709e98d2b0cb6250d2887ef
-            const BAT = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x0d8775f648430679a709e98d2b0cb6250d2887ef&address=0xce5dcffb2aa50deeb229881a294f0b79d41baaeb&tag=latest&apikey=PSHAVQ37TYFF7R369R4X5YJ49Q1WCISQ4Q').then(response => response.json());
+            const BAT = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x0d8775f648430679a709e98d2b0cb6250d2887ef&address=' + DAOWallet + '&tag=latest&apikey=' + EtherScanKey).then(response => response.json());
             // contract address for BNT 0x1f573d6fb3f13d689ff844b4ce37794d79a7ff1c
-            const BNT = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x1f573d6fb3f13d689ff844b4ce37794d79a7ff1c&address=0xce5dcffb2aa50deeb229881a294f0b79d41baaeb&tag=latest&apikey=PSHAVQ37TYFF7R369R4X5YJ49Q1WCISQ4Q').then(response => response.json());
+            const BNT = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x1f573d6fb3f13d689ff844b4ce37794d79a7ff1c&address=' + DAOWallet + '&tag=latest&apikey=' + EtherScanKey).then(response => response.json());
             // contract address for KNC 0xdd974d5c2e2928dea5f71b9825b8b646686bd200
-            const KNC = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xdd974d5c2e2928dea5f71b9825b8b646686bd200&address=0xce5dcffb2aa50deeb229881a294f0b79d41baaeb&tag=latest&apikey=PSHAVQ37TYFF7R369R4X5YJ49Q1WCISQ4Q').then(response => response.json());
+            const KNC = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xdd974d5c2e2928dea5f71b9825b8b646686bd200&address=' + DAOWallet + '&tag=latest&apikey=' + EtherScanKey).then(response => response.json());
             // contract address for DAI 0x6b175474e89094c44da98b954eedeac495271d0f
-            const DAI = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x6b175474e89094c44da98b954eedeac495271d0f&address=0xce5dcffb2aa50deeb229881a294f0b79d41baaeb&tag=latest&apikey=PSHAVQ37TYFF7R369R4X5YJ49Q1WCISQ4Q').then(response => response.json());
+            const DAI = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x6b175474e89094c44da98b954eedeac495271d0f&address=' + DAOWallet + '&tag=latest&apikey=' + EtherScanKey).then(response => response.json());
             // contract address for LEND 0x80fB784B7eD66730e8b1DBd9820aFD29931aab03
-            const LEND = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x80fB784B7eD66730e8b1DBd9820aFD29931aab03&address=0xce5dcffb2aa50deeb229881a294f0b79d41baaeb&tag=latest&apikey=PSHAVQ37TYFF7R369R4X5YJ49Q1WCISQ4Q').then(response => response.json());
+            const LEND = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x80fB784B7eD66730e8b1DBd9820aFD29931aab03&address=' + DAOWallet + '&tag=latest&apikey=' + EtherScanKey).then(response => response.json());
             
             const wallet = '```ETH          ' + (ETH.result/1000000000000000000) +
                             '\nUSDC         ' + (USDC.result/1000000) +
@@ -91,4 +96,4 @@ reporterBot.on('message', async message =>{
 
 
 // * * * THIS NEEDS TO BE THE LAST LINE IN THE FILE * * * 
-reporterBot.login('NzU0NDM4MjMwMTU3Mjk1Njc5.X10vWg.5YMJtjiYNBAWsEgBz9qV6bchRIs');
+reporterBot.login('NzU0NDM4MjMwMTU3Mjk1Njc5.X10vWg.gYRpuOA8PgwcUPOqSwQeyUXZVJg');
